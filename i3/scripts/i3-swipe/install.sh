e6d5cc2a75b4b18399cd2213e1a27752e47615c9
Arthur Lemaire <arthur.lemaire@epitech.eu>
2021-01-26 02:16:44 +0100

    general improvements

--- i3/scripts/i3-swipe/install.sh
+++ i3/scripts/i3-swipe/install.sh
@@ -4,14 +4,13 @@ if [[ "${USER}" == "root" ]]; then
     exit 1
 fi
 
-pip3 install --user -r requirements.txt
-
 EXEC=i3-swipe.py
 UNIT=i3-swipe.service
 
 # smart detecting user path
-DESTDIR=${DESTDIR:-$(echo $PATH | tr ':' '\n' | grep -F "$HOME" | head -1)}
+DESTDIR=${DESTDIR:-$(echo $PATH | tr ' ' '\n' | grep -F "$HOME" | head -1)}
 cp -v $EXEC $DESTDIR/$EXEC
+
 
 if [[ "$(basename $(readlink $(which init)))" == "systemd" ]]; then
     XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
